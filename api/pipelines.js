17515093db69a5a492fd41fc80e5cca70bb700d2
anonmediabr-ux <anonmediabr@gmail.com>
2026-03-30 21:04:32 -0300

    Create pipelines.js

---
+++ api/pipelines.js
@@ -0,0 +1,7 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  
+  const response = await fetch(`https://crm.rdstation.com/api/v1/deal_pipelines?token=69caff506e1ed50013a5b86d`);
+  const data = await response.json();
+  return res.status(200).json(data);
+}
